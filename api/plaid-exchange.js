20fd86cc2e222f8c80f62e0630bf3c3e4cb0c4c6
parse123ea <EdAhn2000@yahoo.com>
2025-05-10 11:46:51 -0700

    Update plaid-exchange.js

--- api/plaid-exchange.js
+++ api/plaid-exchange.js
@@ -1,1 +1,16 @@
-// placeholder
+export default async function handler(req, res) {
+  const { public_token } = req.body;
+
+  const response = await fetch('https://sandbox.plaid.com/item/public_token/exchange', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      client_id: process.env.PLAID_CLIENT_ID,
+      secret: process.env.PLAID_SECRET,
+      public_token
+    })
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
